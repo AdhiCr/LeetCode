5a07935dd1c48dc924cdb073022813804db7d0ab
AdhiCr <adhicr@gmail.com>
2022-08-04 23:41:54 +0200

    setup_format for Flatten a Multilevel Doubly Linked List
    initial solution to be optimised

--- 2_Linked_List/5_Conclusion/3_Flatten_a_Multilevel_Doubly_Linked_List/solution.py
+++ 2_Linked_List/5_Conclusion/3_Flatten_a_Multilevel_Doubly_Linked_List/solution.py
@@ -14,17 +14,7 @@ class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         curr = head
 
-        while (curr and curr.next):
-            # if curr.prev:
-            #     print(f"Prev_Value: {curr.prev.val}")
-            # else:
-            #     print(None)
-            # print(f"Current_value: {curr.val}")
-            # if curr.next:
-            #     print(f"Next_value: {curr.next.val}")
-            # else:
-            #     print(None)
-            # print("\n")
+        while (curr):
             if curr.child:
                 last_of_child = self.flatten_child(curr.child)
 
@@ -34,84 +24,19 @@ class Solution:
                 curr.next = curr.child
                 curr.child = None
 
-                # print(f"CHILD OF {curr.val} AFTER MERGE: {curr.child}")
-                # print(f"NEXT OF {curr.val} AFTER MERGE: {curr.next.val}")
-
-                curr = last_of_child.next
-                curr.prev = last_of_child
-                # print(f"PREVIOUS OF {curr.val} AFTER MERGE: {curr.prev.val}")
-
-            else:
-
-                # print(f"Current_child_value: {curr.child}")
+                if last_of_child.next:
+                    curr = last_of_child.next
+                    curr.prev = last_of_child
+            elif curr.next:
                 curr = curr.next
-
-            #         curr = head
-        #         while(curr and curr.next):
-        #             # print(f"Current_value: {curr.val}")
-        #             # print(f"Child_Node: {curr.child}")
-
-        #             if curr.prev:
-        #                 print(f"Prev_Value: {curr.prev.val}")
-        #             else:
-        #                 print(None)
-        #             print(f"Current_value: {curr.val}")
-        #             if curr.next:
-        #                 print(f"Next_value: {curr.next.val}")
-        #             else:
-        #                 print(None)
-        #             print("\n")
-
-        #             curr = curr.next
-
-        #         if curr.prev:
-        #             print(f"Prev_Value: {curr.prev.val}")
-        #         else:
-        #             print(None)
-        #         print(f"Current_value: {curr.val}")
-        #         if curr.next:
-        #             print(f"Next_value: {curr.next.val}")
-        #         else:
-        #             print(None)
-        #         print("\n")
-        #         # print(f"Current_value: {curr.val}")
-        #         # print(f"Child_Node: {curr.child}")
-        #         curr = curr.prev
-        #         while(curr and curr.prev):
-
-        #             if curr.prev:
-        #                 print(f"Prev_Value: {curr.prev.val}")
-        #             else:
-        #                 print(None)
-        #             print(f"Current_value: {curr.val}")
-        #             if curr.next:
-        #                 print(f"Next_value: {curr.next.val}")
-        #             else:
-        #                 print(None)
-        #             print("\n")
-        #             # print(f"Current_value: {curr.val}")
-        #             # print(f"Child_Node: {curr.child}")
-        #             curr = curr.prev
-        #         print("Finished_backwards_pass")
-        #         curr = curr.prev
-
+            else:
+                break
         return head
 
     def flatten_child(self, head: 'Optional[Node]') -> 'Optional[Node]':
         curr = head
-        # print(f"entering recursive method with value: {head.val}")
 
-        while (curr and curr.next):
-            # if curr.prev:
-            #     print(f"Prev_Value: {curr.prev.val}")
-            # else:
-            #     print(None)
-            # print(f"Current_value: {curr.val}")
-            # if curr.next:
-            #     print(f"Next_value: {curr.next.val}")
-            # else:
-            #     print(None)
-            # print("\n")
+        while (curr):
             if curr.child:
                 last_of_child = self.flatten_child(curr.child)
 
@@ -121,19 +46,11 @@ class Solution:
                 curr.next = curr.child
                 curr.child = None
 
-                # print(f"CHILD OF {curr.val} AFTER MERGE: {curr.child}")
-                # print(f"NEXT OF {curr.val} AFTER MERGE: {curr.next.val}")
-
-                curr = last_of_child.next
-                curr.prev = last_of_child
-                # print(f"PREVIOUS OF {curr.val} AFTER MERGE: {curr.prev.val}")
-            else:
-
-                # print(f"Current_child_value: {curr.child}")
+                if last_of_child.next:
+                    curr = last_of_child.next
+                    curr.prev = last_of_child
+            elif curr.next:
                 curr = curr.next
-
-        # print(f"Current_value: {curr.val}")
-
-        # print(f"Current_child_value: {curr.child}")
-        # print(f"exiting recursive method with value: {curr.val}")
+            else:
+                break
         return curr
