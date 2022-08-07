e23652f4fbf632cee0d270c2071fd518005bf41b
AdhiCr <adhicr@gmail.com>
2022-08-08 00:21:25 +0200

    code currently works only on the test cases
    fails for [[7,null],[13,0],[11,4],[10,2],[1,0]] to be investigated

--- 2_Linked_List/5_Conclusion/5_Copy_List_with_Random_Pointer/solution.py
+++ 2_Linked_List/5_Conclusion/5_Copy_List_with_Random_Pointer/solution.py
@@ -8,5 +8,18 @@ class Node:
         self.random = random
 
 class Solution:
-    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
-        pass
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+
+        hash_map = {None: None}
+        og_curr = head
+        while (og_curr):
+            hash_map[og_curr] = Node(x=og_curr.val)
+            og_curr = og_curr.next
+
+        og_curr = head
+        while (og_curr.next):
+            hash_map[og_curr].next = hash_map[og_curr.next]
+            hash_map[og_curr].random = hash_map[og_curr.random]
+            og_curr = og_curr.next
+
+        return hash_map[head]
