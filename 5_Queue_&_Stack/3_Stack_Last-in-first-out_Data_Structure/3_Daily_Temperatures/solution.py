0ca66c41f3fcb4f9bcff3a4c14875cc82da75c64
AdhiCr <adhicr@gmail.com>
2022-09-14 23:17:34 +0200

    Solution for Daily Temperatures

--- 5_Queue_&_Stack/3_Stack_Last-in-first-out_Data_Structure/3_Daily_Temperatures/solution.py
+++ 5_Queue_&_Stack/3_Stack_Last-in-first-out_Data_Structure/3_Daily_Temperatures/solution.py
@@ -3,8 +3,19 @@ from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        pass
+        res = [0] * len(temperatures)
+
+        stack = []
+        for i, v in enumerate(temperatures):
+            while stack and stack[-1][1] < v:
+                index, value = stack.pop()
+                res[index] = i - index
+            stack.append([i, v])
+        return res
+
 
 
 dummy = Solution()
-print(dummy.dailyTemperatures([]))
+print(dummy.dailyTemperatures([73,74,75,71,69,72,76,73]))
+print(dummy.dailyTemperatures([30,40,50,60]))
+print(dummy.dailyTemperatures([30,60,90]))
