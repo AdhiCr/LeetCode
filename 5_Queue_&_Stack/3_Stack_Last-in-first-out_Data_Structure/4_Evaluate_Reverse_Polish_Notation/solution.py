ce6c96b23c8cfd721280a1f0149b5054c5f97706
AdhiCr <adhicr@gmail.com>
2022-09-16 21:26:58 +0200

    solution for Evaluate Reverse Polish Notation

--- 5_Queue_&_Stack/3_Stack_Last-in-first-out_Data_Structure/4_Evaluate_Reverse_Polish_Notation/solution.py
+++ 5_Queue_&_Stack/3_Stack_Last-in-first-out_Data_Structure/4_Evaluate_Reverse_Polish_Notation/solution.py
@@ -3,4 +3,23 @@ from typing import List
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        pass
+        stack = []
+        for i in tokens:
+            if i == "+":
+                stack[-1] = stack[-2] + stack.pop()
+            elif i == "-":
+                stack[-1] = stack[-2] - stack.pop()
+            elif i == "*":
+                stack[-1] = stack[-2] * stack.pop()
+            elif i == "/":
+                stack[-1] = int(stack[-2] / stack.pop())
+            else:
+                stack.append(int(i))
+
+        return stack.pop()
+
+dummy = Solution()
+print(dummy.evalRPN(["2","1","+","3","*"]))
+print(dummy.evalRPN(["4","13","5","/","+"]))
+print(dummy.evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+print(dummy.evalRPN(["0","3","/"]))
