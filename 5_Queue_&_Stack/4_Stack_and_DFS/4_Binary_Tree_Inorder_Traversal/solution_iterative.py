7e0366df8589fb97f983401644e54246810c8a72
AdhiCr <adhicr@gmail.com>
2022-09-23 22:48:41 +0200

    solution for  Binary Tree Inorder Traversal
    iterative solution

---
+++ 5_Queue_&_Stack/4_Stack_and_DFS/4_Binary_Tree_Inorder_Traversal/solution_iterative.py
@@ -0,0 +1,29 @@
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = []
+        curr_node = root
+
+        while curr_node or stack:
+            while curr_node:
+                stack.append(curr_node)
+                curr_node = curr_node.left
+            curr_node = stack.pop()
+            result.append(curr_node.val)
+            curr_node = curr_node.right
+        return result
+
+
+
+
+
--- 5_Queue_&_Stack/4_Stack_and_DFS/4_Binary_Tree_Inorder_Traversal/solution_recursive.py
+++ 5_Queue_&_Stack/4_Stack_and_DFS/4_Binary_Tree_Inorder_Traversal/solution_recursive.py
@@ -22,5 +22,3 @@ class Solution:
 
         recursive_traversal(root)
         return result
-
-
