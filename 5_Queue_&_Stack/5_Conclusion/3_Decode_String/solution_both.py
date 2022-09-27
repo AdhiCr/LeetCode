289763afd75a601ead0f11decd95e52191ad1b5e
AdhiCr <adhicr@gmail.com>
2022-09-27 22:37:07 +0200

    solution for Decode String

--- 5_Queue_&_Stack/5_Conclusion/3_Decode_String/solution_both.py
+++ 5_Queue_&_Stack/5_Conclusion/3_Decode_String/solution_both.py
@@ -9,15 +9,15 @@ class Solution:
                 while stack[-1] != "[":
                     sub_decoder = stack.pop() + sub_decoder
                 stack.pop()
-                while len(stack) > 0 and stack[-1] in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                while stack and stack[-1].isdigit():
                     multiplier = stack.pop() + multiplier
                 stack.append(sub_decoder * int(multiplier))
 
         return "".join(stack)
 
 dummy = Solution()
-# print(dummy.decodeString(s="3[a]2[bc]"))
-# print(dummy.decodeString(s="3[a2[c]]"))
-# print(dummy.decodeString(s="2[abc]3[cd]ef"))
+print(dummy.decodeString(s="3[a]2[bc]"))
+print(dummy.decodeString(s="3[a2[c]]"))
+print(dummy.decodeString(s="2[abc]3[cd]ef"))
 print(dummy.decodeString(s="10[lc]"))
 
