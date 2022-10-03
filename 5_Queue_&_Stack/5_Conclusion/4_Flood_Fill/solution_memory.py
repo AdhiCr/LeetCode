2686e5ed466a30f604300edc684fa391c004138d
AdhiCr <adhicr@gmail.com>
2022-10-03 20:55:26 +0200

    solution for Flood Fill
    memory efficient solution

--- 5_Queue_&_Stack/5_Conclusion/4_Flood_Fill/solution_memory.py
+++ 5_Queue_&_Stack/5_Conclusion/4_Flood_Fill/solution_memory.py
@@ -2,21 +2,21 @@ from typing import List
 
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        targetColor = image[sr][sc]
+        old_color = image[sr][sc]
         row, col = len(image), len(image[0])
 
-        def dfs(r: int, c: int):
-            if r < 0 or r >= row or c < 0 or c >= col:
-                return
-
-            if image[r][c] == color or image[r][c] != targetColor:
-                return
-
-            image[r][c] = color
-            dfs(sr + 1, sc)
-            dfs(sr - 1, sc)
-            dfs(sr, sc + 1)
-            dfs(sr, sc - 1)
-
-        dfs(sr, sc)
+        self.dfs(old_color, image, sr, sc, row, col, color)
         return image
+
+    def dfs(self, old_color: int, image: List[List[int]], r: int, c: int, row: int, col: int, color: int):
+        if r < 0 or r >= row or c < 0 or c >= col:
+            return
+
+        if image[r][c] == color or image[r][c] != old_color:
+            return
+
+        image[r][c] = color
+        self.dfs(old_color, image, r + 1, c, row, col, color)
+        self.dfs(old_color, image, r - 1, c, row, col, color)
+        self.dfs(old_color, image, r, c - 1, row, col, color)
+        self.dfs(old_color, image, r, c + 1, row, col, color)
