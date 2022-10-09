16b29a8b3de34861f16cb14e191e7ade8e1dedd3
AdhiCr <adhicr@gmail.com>
2022-10-09 11:26:17 +0200

    solution for Insertion Sort List
    to be optimised

---
+++ 6_Sorting/2_Comparison_Based_Sort/3_Insertion_Sort_List/solution.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_head = ListNode(next=head)
+        p_1, p_2 = head, head.next
+
+        while p_2:
+            if p_2.val >= p_1.val:
+                p_1, p_2 = p_2, p_2.next
+                continue
+
+            dum_p = dummy_head
+            while p_2.val > dum_p.next.val:
+                dum_p = dum_p.next
+
+            p_1.next = p_2.next
+            p_2.next = dum_p.next
+            dum_p.next = p_2
+            p_2 = p_1.next
+
+        return dummy_head.next
+
