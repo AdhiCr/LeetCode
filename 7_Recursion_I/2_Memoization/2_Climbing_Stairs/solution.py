f87468f70aea3b6680c52ff5f086f21465c3916b
AdhiCr <adhicr@gmail.com>
2022-11-06 18:28:21 +0100

    directory for Climbing Stairs

---
+++ 7_Recursion_I/2_Memoization/2_Climbing_Stairs/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def __init__(self):
+        self.step_ways = 0
+
+    def climbStairs(self, n: int) -> int:
+        pass
+
+
+dummy = Solution()
+print(dummy.climbStairs(2))
+print(dummy.climbStairs(3))
