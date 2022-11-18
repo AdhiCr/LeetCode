f3ed8f7f745c7c2ec7c94c77c7cd5ec00ec29a1d
AdhiCr <adhicr@gmail.com>
2022-11-18 20:31:32 +0100

    solution for Binary Search

--- 8_Binary_Search/1_Background/1_Binary_Search/solution_both.py
+++ 8_Binary_Search/1_Background/1_Binary_Search/solution_both.py
@@ -22,6 +22,20 @@ class Solution:
         else:
             return -1
 
+# Equivalent and more elegant solution
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         left, right = 0, len(nums) - 1
+#         while left <= right:
+#             mid = (left+right)//2
+#             if target == nums[mid]:
+#                 return mid
+#             elif target < nums[mid]:
+#                 right = mid - 1
+#             elif target > nums[mid]:
+#                 left = mid + 1
+#         return -1
+
 dummy = Solution()
 print(dummy.search(nums = [-1,0,3,5,9,12], target = 9))
 print(dummy.search([-1,0,3,5,9,12], target = 2))
