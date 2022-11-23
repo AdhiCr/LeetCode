bcf0e1a466a4b6eac0d4abaacf7919c3569051f7
AdhiCr <adhicr@gmail.com>
2022-11-23 23:47:22 +0100

    solution for First Bad Version
    to explore memory optimisation

--- 8_Binary_Search/3_Template_II/1_First_Bad_Version/solution.py
+++ 8_Binary_Search/3_Template_II/1_First_Bad_Version/solution.py
@@ -2,5 +2,20 @@
 # def isBadVersion(version: int) -> bool:
 
 class Solution:
-    def firstBadVersion(self, n: int) -> int:
-        pass
+    def firstBadVersion(self, n: int, bad_version: int) -> int:
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            if mid == bad_version:
+                right = mid
+            else:
+                left = mid + 1
+
+        if left == bad_version:
+            return left
+
+
+dummy = Solution()
+print(dummy.firstBadVersion(5,4))
+print(dummy.firstBadVersion(1,1))
+print(dummy.firstBadVersion(9,9))
