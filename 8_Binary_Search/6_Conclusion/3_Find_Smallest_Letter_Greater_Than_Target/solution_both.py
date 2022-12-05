4aedf7ddc744f31c16f3833c6240fd25dc2a9f22
AdhiCr <adhicr@gmail.com>
2022-12-05 22:56:11 +0100

    solution for Find Smallest Letter Greater Than Target

---
+++ 8_Binary_Search/6_Conclusion/3_Find_Smallest_Letter_Greater_Than_Target/solution_both.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters)-1
+
+        while left <= right:
+            mid = (left+right)//2
+            if target < letters[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return letters[left%len(letters)]
