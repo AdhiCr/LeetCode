1b41c0c27d042a666363a0eda8f9d56a052e8f47
AdhiCr <adhicr@gmail.com>
2023-01-26 23:37:19 +0100

    solution for solution_both.py

--- 11_Binary_Search_Tree/3_Conclusion/1_Kth_Largest_Element_in_a_Stream/solution_both.py
+++ 11_Binary_Search_Tree/3_Conclusion/1_Kth_Largest_Element_in_a_Stream/solution_both.py
@@ -1,14 +1,13 @@
 import heapq
 from typing import List
 
-class KthLargest:
 
+class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.minHeap, self.k = nums, k
         heapq.heapify(self.minHeap)
         while len(self.minHeap) > k:
             heapq.heappop(self.minHeap)
-            
 
     def add(self, val: int) -> int:
         heapq.heappush(self.minHeap, val)
@@ -18,5 +17,9 @@ class KthLargest:
 
 
 # Your KthLargest object will be instantiated and called as such:
-# obj = KthLargest(k, nums)
-# param_1 = obj.add(val)
+obj = KthLargest(3,[4,5,8,2])
+print(obj.add(3))
+print(obj.add(5))
+print(obj.add(10))
+print(obj.add(9))
+print(obj.add(4))
--- 11_Binary_Search_Tree/3_Conclusion/2_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/solution_both.py
+++ 11_Binary_Search_Tree/3_Conclusion/2_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/solution_both.py
@@ -8,4 +8,10 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pass
+        while root:
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
