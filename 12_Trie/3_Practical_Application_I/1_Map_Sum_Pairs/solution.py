a66d754bb765bd3a5a48d6c30a2e1f0dabe9cd2c
AdhiCr <adhicr@gmail.com>
2023-02-03 22:00:56 +0100

    directory for Map Sum Pairs

---
+++ 12_Trie/3_Practical_Application_I/1_Map_Sum_Pairs/solution.py
@@ -0,0 +1,15 @@
+class MapSum:
+
+    def __init__(self):
+        pass
+
+    def insert(self, key: str, val: int) -> None:
+        pass
+
+    def sum(self, prefix: str) -> int:
+        pass
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
