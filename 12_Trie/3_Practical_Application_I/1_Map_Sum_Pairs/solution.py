41a925b6ffafb632d5797f8113f9a99e0105f105
AdhiCr <adhicr@gmail.com>
2023-02-04 23:50:46 +0100

    WIP:solution for Map Sum Pairs

--- 12_Trie/3_Practical_Application_I/1_Map_Sum_Pairs/solution.py
+++ 12_Trie/3_Practical_Application_I/1_Map_Sum_Pairs/solution.py
@@ -1,15 +1,42 @@
+class TrieNode:
+    def __init__(self, sum_val=0):
+        self.children = dict()
+        self.sum_val = sum_val
+
+
 class MapSum:
 
     def __init__(self):
-        pass
+        self.root = TrieNode()
 
     def insert(self, key: str, val: int) -> None:
-        pass
+        cur = self.root
+        cur.sum_val += val
+        for c in key:
+            if c not in cur.children:
+                cur.children[c] = TrieNode(val)
+            elif c in cur.children:
+                cur.children[c].sum_val += val
+            cur = cur.children[c]
 
     def sum(self, prefix: str) -> int:
-        pass
+        cur = self.root
+        sum_val = 0
+        for c in prefix:
+            if c in cur.children:
+                sum_val = cur.children[c].sum_val
+                cur = cur.children[c]
+            else:
+                sum_val = 0
+        return sum_val
 
 # Your MapSum object will be instantiated and called as such:
 # obj = MapSum()
 # obj.insert(key,val)
 # param_2 = obj.sum(prefix)
+
+dummy = MapSum()
+print(dummy.insert("apple", 3))
+print(dummy.sum("ap"))
+print(dummy.insert("app", 2))
+print(dummy.sum("ap"))
