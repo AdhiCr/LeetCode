0b47b9fadb1ec10dafb0e75f553449f5842a5886
AdhiCr <adhicr@gmail.com>
2023-02-17 22:22:47 +0100

    solution for Word Search II
    code directly taken from internet, better solution to be analysed and found

--- 12_Trie/4_Practical_Application_II/2_Word_Search_II/solution.py
+++ 12_Trie/4_Practical_Application_II/2_Word_Search_II/solution.py
@@ -1,46 +1,56 @@
-from typing import List
+# Code source: https://walkccc.me/LeetCode/problems/0212/
 
+from typing import Dict, List, Optional
+import collections
 
 class TrieNode:
     def __init__(self):
-        self.children = dict()
-        self.end_of_word = False
-
-    def add_word(self, word):
-        curr = self
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.end_of_word = True
+        self.children: Dict[str, TrieNode] = collections.defaultdict(TrieNode)
+        self.word: Optional[str] = None
 
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m = len(board)
+        n = len(board[0])
+        ans = []
         root = TrieNode()
+
+        def insert(word: str) -> None:
+            node = root
+            for c in word:
+                if c not in node.children:
+                    node.children[c] = TrieNode()
+                node = node.children[c]
+            node.word = word
+
         for word in words:
-            root.add_word(word)
+            insert(word)
 
-        rows, cols = len(board), len(board[0])
-        res, visit = set(), set()
-
-        def dfs(r, c, node, word):
-            if r < 0 or c < 0 or r == rows or c == cols or (r, c) in visit or board[r][c] not in node.children:
+        def dfs(i: int, j: int, node: TrieNode) -> None:
+            if i < 0 or i == m or j < 0 or j == n:
+                return
+            if board[i][j] == '*':
                 return
 
-            visit.add((r, c))
-            node = node.children[board[r][c]]
-            word += board[r][c]
-            if node.end_of_word:
-                res.add(word)
-            rc_combo = [(r-1, c), (r+1, c), (r, c-1), (r, c+1)]
-            for rc in rc_combo:
-                dfs(rc[0], rc[1], node, word)
-            visit.remove((r, c))
+            c = board[i][j]
+            if c not in node.children:
+                return
 
-        for r in range(rows):
-            for c in range(cols):
-                dfs(r, c, root, "")
+            child = node.children[c]
+            if child.word:
+                ans.append(child.word)
+                child.word = None
 
-        return list(res)
+            board[i][j] = '*'
+            dfs(i + 1, j, child)
+            dfs(i - 1, j, child)
+            dfs(i, j + 1, child)
+            dfs(i, j - 1, child)
+            board[i][j] = c
 
+        for i in range(m):
+            for j in range(n):
+                dfs(i, j, root)
+
+        return ans
